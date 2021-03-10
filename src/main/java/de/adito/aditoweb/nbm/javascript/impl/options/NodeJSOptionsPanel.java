6f78c109976697889faa70d10521f7b4058f2fc2
Werner Glanzer <w.glanzer@adito.de>
2021-03-10 12:59:01 +0100

    feat: added download dialog to download nodejs versions

--- src/main/java/de/adito/aditoweb/nbm/javascript/impl/options/NodeJSOptionsPanel.java
+++ src/main/java/de/adito/aditoweb/nbm/javascript/impl/options/NodeJSOptionsPanel.java
@@ -1,17 +1,25 @@
 package de.adito.aditoweb.nbm.javascript.impl.options;
 
+import de.adito.aditoweb.nbm.javascript.impl.options.downloader.INodeJSDownloader;
 import de.adito.aditoweb.nbm.javascript.impl.version.NodeJSVersionFactory;
 import de.adito.aditoweb.nbm.nbide.nbaditointerface.javascript.node.INodeJSVersion;
 import de.adito.swing.*;
 import info.clearthought.layout.TableLayout;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.*;
+import org.netbeans.api.progress.BaseProgressUtils;
+import org.openide.*;
 import org.openide.util.NbBundle;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.event.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.List;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.*;
 
 /**
  * Panel to display the options
@@ -21,7 +29,7 @@ import java.util.concurrent.CompletableFuture;
 public class NodeJSOptionsPanel extends JPanel
 {
 
-  private final JTextField path = new JTextField();
+  private final _PathSelection path;
   private NodeJSOptions options;
 
   @NbBundle.Messages({
@@ -51,11 +59,9 @@ public class NodeJSOptionsPanel extends JPanel
     tlu.add(0, 0, 2, 0, new LinedDecorator(Bundle.LBL_Installation(), null));
 
     // Path
-    JPanel pathContainer = new JPanel(new BorderLayout(gap, 0));
-    pathContainer.add(path, BorderLayout.CENTER);
-    pathContainer.add(_createBrowseButton(), BorderLayout.EAST);
+    path = new _PathSelection(_createDownloadButton());
     tlu.add(0, 2, new JLabel(Bundle.LBL_Path() + ":"));
-    tlu.add(2, 2, pathContainer);
+    tlu.add(2, 2, path);
 
     // Version reader
     tlu.add(2, 4, _createVersionLabel());
@@ -69,7 +75,7 @@ public class NodeJSOptionsPanel extends JPanel
   public void setCurrent(@NotNull NodeJSOptions pOptions)
   {
     options = pOptions;
-    path.setText(pOptions.getPath());
+    path.setValue(pOptions.getPath());
   }
 
   /**
@@ -79,28 +85,75 @@ public class NodeJSOptionsPanel extends JPanel
   public NodeJSOptions getCurrent()
   {
     return options.toBuilder()
-        .path(path.getText())
+        .path(path.getValue())
         .build();
   }
 
   /**
-   * @return creates the browse button to search for a valid nodejs installation
+   * @return creates the button to download a nodejs version
    */
   @NbBundle.Messages({
-      "LBL_Browse=Browse...",
-      "LBL_ChooserTitle=Locate NodeJS Executable"
+      "LBL_Download=Download...",
+      "LBL_DownloadTitle=Select Version to Download",
+      "LBL_DownloadBtn=Download",
+      "LBL_DownloadingVersion=Downloading NodeJS ({0}, {1})..."
   })
   @NotNull
-  private JButton _createBrowseButton()
+  private JButton _createDownloadButton()
   {
-    JButton btn = new JButton(Bundle.LBL_Browse());
+    JButton btn = new JButton(Bundle.LBL_Download());
     btn.addActionListener(e -> {
-      JFileChooser chooser = new JFileChooser(path.getText());
-      chooser.setDialogTitle(Bundle.LBL_ChooserTitle());
-      chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-      int result = chooser.showOpenDialog(NodeJSOptionsPanel.this);
-      if (result == JFileChooser.APPROVE_OPTION && chooser.getSelectedFile() != null)
-        path.setText(chooser.getSelectedFile().getAbsolutePath());
+      try
+      {
+        INodeJSDownloader downloader = INodeJSDownloader.getInstance();
+        _DownloadPanel container = new _DownloadPanel(downloader.getAvailableVersions());
+
+        // validity check
+        Supplier<Boolean> isValid = () -> container.getVersion() != null && container.getTarget() != null;
+
+        // action listener
+        ActionListener onAction = action -> {
+          if (Objects.equals(action.getActionCommand(), Bundle.LBL_DownloadBtn()) &&
+              container.getVersion() != null && container.getTarget() != null)
+          {
+            // Async
+            BaseProgressUtils.showProgressDialogAndRun(() -> {
+              try
+              {
+                File bin = downloader.downloadVersion(container.getVersion(), container.getTarget());
+                path.setValue(bin.getAbsolutePath());
+              }
+              catch (Exception ex2)
+              {
+                throw new RuntimeException("Failed to download nodejs binary", ex2);
+              }
+            }, Bundle.LBL_DownloadingVersion(container.getVersion(), container.getTarget().getAbsolutePath()));
+          }
+        };
+
+        // create dialog
+        JButton downloadBtn = new JButton(Bundle.LBL_DownloadBtn());
+        DialogDescriptor descriptor = new DialogDescriptor(container, Bundle.LBL_DownloadTitle(), true,
+                                                           new Object[]{downloadBtn, DialogDescriptor.CANCEL_OPTION},
+                                                           downloadBtn, DialogDescriptor.DEFAULT_ALIGN, null,
+                                                           onAction);
+
+        // configure descriptor
+        descriptor.setClosingOptions(new Object[]{downloadBtn, DialogDescriptor.CANCEL_OPTION});
+
+        // set valid, if changed
+        downloadBtn.setEnabled(isValid.get());
+        container.addChangeListener(a -> downloadBtn.setEnabled(isValid.get()));
+
+        Dialog dialog = DialogDisplayer.getDefault().createDialog(descriptor);
+        dialog.setMinimumSize(new Dimension(450, 150));
+        dialog.setPreferredSize(getMinimumSize());
+        dialog.setVisible(true);
+      }
+      catch (Exception ex)
+      {
+        throw new RuntimeException("Failed to download nodejs binary", ex);
+      }
     });
     return btn;
   }
@@ -120,7 +173,7 @@ public class NodeJSOptionsPanel extends JPanel
     Runnable updateLabel = () -> _getNodeJSVersionOfSelectedPath(Bundle.LBL_UnknownVersion())
         .thenAccept(pVersion -> label.setText(Bundle.LBL_Version() + pVersion));
     updateLabel.run();
-    path.getDocument().addDocumentListener(new DocumentListener()
+    path.path.getDocument().addDocumentListener(new DocumentListener()
     {
       @Override
       public void insertUpdate(DocumentEvent e)
@@ -155,7 +208,7 @@ public class NodeJSOptionsPanel extends JPanel
     return CompletableFuture.supplyAsync(() -> {
       try
       {
-        INodeJSVersion version = NodeJSVersionFactory.create(new File(path.getText()));
+        INodeJSVersion version = NodeJSVersionFactory.create(new File(path.getValue()));
         if (version != null)
           return version.getVersion();
       }
@@ -166,6 +219,161 @@ public class NodeJSOptionsPanel extends JPanel
 
       return pDefault;
     });
+  }
+
+  /**
+   * Checks, if the given path is a valid file path
+   *
+   * @param pPath Path to check
+   * @return true, if valid
+   */
+  private static boolean _isValidFilePath(@NotNull String pPath)
+  {
+    try
+    {
+      //noinspection ResultOfMethodCallIgnored
+      new File(pPath).getCanonicalPath();
+      return true;
+    }
+    catch (Exception e)
+    {
+      return false;
+    }
+  }
+
+  /**
+   * Component to select a path
+   */
+  private static class _PathSelection extends JPanel
+  {
+    private final JTextField path = new JTextField();
+
+    public _PathSelection(JButton... pAdditionalButtons)
+    {
+      super(new BorderLayout(5, 0));
+      JPanel panel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 5, 0));
+      panel.add(_createBrowseButton(this, path::getText, path::setText));
+      for (JButton btn : pAdditionalButtons)
+        panel.add(btn);
+      add(path, BorderLayout.CENTER);
+      add(panel, BorderLayout.EAST);
+    }
+
+    public void setValue(@NotNull String pValue)
+    {
+      path.setText(pValue);
+    }
+
+    @NotNull
+    public String getValue()
+    {
+      return path.getText();
+    }
+
+    /**
+     * @return creates the browse button to search for a valid nodejs installation
+     */
+    @NbBundle.Messages({
+        "LBL_Browse=Browse...",
+        "LBL_ChooserTitle=Locate NodeJS Executable"
+    })
+    @NotNull
+    private static JButton _createBrowseButton(@NotNull JComponent pParent, @NotNull Supplier<String> pDefaultPath, @NotNull Consumer<String> pOnFileSelected)
+    {
+      JButton btn = new JButton(Bundle.LBL_Browse());
+      btn.addActionListener(e -> {
+        JFileChooser chooser = new JFileChooser(pDefaultPath.get());
+        chooser.setDialogTitle(Bundle.LBL_ChooserTitle());
+        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        int result = chooser.showOpenDialog(pParent);
+        if (result == JFileChooser.APPROVE_OPTION && chooser.getSelectedFile() != null)
+          pOnFileSelected.accept(chooser.getSelectedFile().getAbsolutePath());
+      });
+      return btn;
+    }
+  }
+
+  /**
+   * Panel for Download Dialog
+   */
+  private static class _DownloadPanel extends JPanel
+  {
+    private static final String _DEFAULT_PATH = System.getProperty("user.home") + "/.nodejs-versions";
+    private final JComboBox<String> versions;
+    private final _PathSelection path;
+
+    @NbBundle.Messages({
+        "LBL_TargetPath=Target Path: "
+    })
+    public _DownloadPanel(@NotNull List<String> pAvailableVersions)
+    {
+      setBorder(new EmptyBorder(5, 5, 5, 5));
+
+      double fill = TableLayout.FILL;
+      double pref = TableLayout.PREFERRED;
+      int gap = 5;
+
+      double[] cols = {pref, gap, pref, fill};
+      double[] rows = {pref,
+                       gap,
+                       pref};
+
+      setLayout(new TableLayout(cols, rows));
+      TableLayoutUtil tlu = new TableLayoutUtil(this);
+
+      // version
+      versions = new JComboBox<>(new DefaultComboBoxModel<>(pAvailableVersions.toArray(new String[0])));
+      versions.setSelectedIndex(0); //select "latest"
+      versions.setEditable(false);
+      tlu.add(0, 0, new JLabel(Bundle.LBL_Version()));
+      tlu.add(2, 0, versions);
+
+      // path
+      path = new _PathSelection();
+      path.path.setText(_DEFAULT_PATH);
+      tlu.add(0, 2, new JLabel(Bundle.LBL_TargetPath()));
+      tlu.add(2, 2, 3, 2, path);
+    }
+
+    @Nullable
+    public String getVersion()
+    {
+      return (String) versions.getSelectedItem();
+    }
+
+    @Nullable
+    public File getTarget()
+    {
+      if (path.getValue().trim().isEmpty() || !_isValidFilePath(path.getValue()) || !new File(path.getValue()).isAbsolute())
+        return null;
+      return new File(path.getValue());
+    }
+
+    public void addChangeListener(@NotNull ChangeListener pOnChange)
+    {
+      path.path.getDocument().addDocumentListener(new DocumentListener()
+      {
+        @Override
+        public void insertUpdate(DocumentEvent e)
+        {
+          pOnChange.stateChanged(new ChangeEvent(path));
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent e)
+        {
+          pOnChange.stateChanged(new ChangeEvent(path));
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent e)
+        {
+          pOnChange.stateChanged(new ChangeEvent(path));
+        }
+      });
+
+      versions.addItemListener(e -> pOnChange.stateChanged(new ChangeEvent(versions)));
+    }
   }
 
 }
