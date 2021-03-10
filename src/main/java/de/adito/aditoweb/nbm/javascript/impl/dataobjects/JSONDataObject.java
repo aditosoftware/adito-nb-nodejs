fcbd3f1a5e4d1d82dcde7e17b215da8962824dcc
Werner Glanzer <w.glanzer@adito.de>
2021-03-10 14:54:30 +0100

    feat: added tm language for JavaScript, TypeScript and JSON

---
+++ src/main/java/de/adito/aditoweb/nbm/javascript/impl/dataobjects/JSONDataObject.java
@@ -0,0 +1,34 @@
+package de.adito.aditoweb.nbm.javascript.impl.dataobjects;
+
+import org.netbeans.modules.textmate.lexer.api.GrammarRegistration;
+import org.openide.filesystems.*;
+import org.openide.loaders.*;
+import org.openide.util.NbBundle;
+
+import java.io.IOException;
+
+/**
+ * DataObject for JSON
+ *
+ * @author w.glanzer, 10.03.2021
+ */
+@NbBundle.Messages("LBL_JSONDataObject_LOADER=JSON")
+@MIMEResolver.ExtensionRegistration(displayName = "#LBL_JSONDataObject_LOADER", mimeType = "text/json", extension = "json")
+@DataObject.Registration(mimeType = "text/json", displayName = "#LBL_JSONDataObject_LOADER", position = 300)
+@GrammarRegistration(mimeType = "text/json", grammar = "JSON.tmLanguage.json")
+public class JSONDataObject extends MultiDataObject
+{
+
+  public JSONDataObject(FileObject pf, MultiFileLoader loader) throws IOException
+  {
+    super(pf, loader);
+    registerEditor("text/json", false);
+  }
+
+  @Override
+  protected int associateLookup()
+  {
+    return 1;
+  }
+
+}
---
+++ src/main/java/de/adito/aditoweb/nbm/javascript/impl/dataobjects/TypeScriptDataObject.java
@@ -0,0 +1,34 @@
+package de.adito.aditoweb.nbm.javascript.impl.dataobjects;
+
+import org.netbeans.modules.textmate.lexer.api.GrammarRegistration;
+import org.openide.filesystems.*;
+import org.openide.loaders.*;
+import org.openide.util.NbBundle;
+
+import java.io.IOException;
+
+/**
+ * DataObject for TypeScript
+ *
+ * @author w.glanzer, 10.03.21
+ */
+@NbBundle.Messages("LBL_TypeScriptDataObject_LOADER=TypeScript")
+@MIMEResolver.ExtensionRegistration(displayName = "#LBL_TypeScriptDataObject_LOADER", mimeType = "text/typescript", extension = "ts", position = 193)
+@DataObject.Registration(mimeType = "text/typescript", displayName = "#LBL_TypeScriptDataObject_LOADER", position = 300)
+@GrammarRegistration(mimeType = "text/typescript", grammar = "TypeScript.tmLanguage.json")
+public class TypeScriptDataObject extends MultiDataObject
+{
+
+  public TypeScriptDataObject(FileObject pf, MultiFileLoader loader) throws IOException
+  {
+    super(pf, loader);
+    registerEditor("text/typescript", false);
+  }
+
+  @Override
+  protected int associateLookup()
+  {
+    return 1;
+  }
+
+}
