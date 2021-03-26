36685b96cd50b9b0501d796ad13f282f7153d2ba
Werner Glanzer <w.glanzer@adito.de>
2021-03-26 08:26:10 +0100

    feat: provider should listen on nodejs path, packagejson and tsconfig to trigger observable, that a new environment is ready

--- src/main/java/de/adito/aditoweb/nbm/javascript/impl/NodeJSProviderImpl.java
+++ src/main/java/de/adito/aditoweb/nbm/javascript/impl/NodeJSProviderImpl.java
@@ -3,15 +3,16 @@ package de.adito.aditoweb.nbm.javascript.impl;
 import de.adito.aditoweb.nbm.javascript.impl.options.NodeJSOptions;
 import de.adito.aditoweb.nbm.javascript.impl.version.NodeJSEnvironmentFactory;
 import de.adito.aditoweb.nbm.nbide.nbaditointerface.javascript.node.*;
+import de.adito.observables.netbeans.FileObservable;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.disposables.*;
-import io.reactivex.rxjava3.subjects.ReplaySubject;
 import org.jetbrains.annotations.NotNull;
 import org.netbeans.api.project.Project;
 import org.openide.util.lookup.ServiceProvider;
 
 import java.io.File;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author w.glanzer, 05.03.2021
@@ -19,13 +20,8 @@ import java.util.Optional;
 @ServiceProvider(service = INodeJSProvider.class, path = "Projects/de-adito-project/Lookup")
 public class NodeJSProviderImpl implements INodeJSProvider, Disposable
 {
-
   private final Project project;
   private final CompositeDisposable compositeDisposable = new CompositeDisposable();
-  private final ReplaySubject<Optional<INodeJSEnvironment>> current = ReplaySubject.create();
-  private final Observable<Optional<INodeJSEnvironment>> current_observe = current
-      .replay(1)
-      .autoConnect(1, compositeDisposable::add);
 
   @SuppressWarnings("unused") // ServiceProvider
   public NodeJSProviderImpl()
@@ -43,8 +39,15 @@ public class NodeJSProviderImpl implements INodeJSProvider, Disposable
   @Override
   public Observable<Optional<INodeJSEnvironment>> current()
   {
-    //todo do something useful
-    return Observable.just(Optional.ofNullable(NodeJSEnvironmentFactory.create(new File(NodeJSOptions.getInstance().getPath()))));
+    return Observable.combineLatest(_observeNodeJSVersion(), _observePackageJson(), _observeTSConfig(),
+                                    (pNodeJSOpt, pPackageJsonOpt, pTSConfigOpt) -> {
+                                      if (pNodeJSOpt.isPresent() && pPackageJsonOpt.isPresent())
+                                        return Optional.ofNullable(NodeJSEnvironmentFactory.create(pNodeJSOpt.get()));
+                                      return Optional.<INodeJSEnvironment>empty();
+                                    })
+
+        // we will throttle, so that the listening server wont trigger too often
+        .throttleLatest(500, TimeUnit.MILLISECONDS);
   }
 
   @Override
@@ -57,6 +60,43 @@ public class NodeJSProviderImpl implements INodeJSProvider, Disposable
   public boolean isDisposed()
   {
     return compositeDisposable.isDisposed();
+  }
+
+  /**
+   * @return Observable that contains the current nodejs version specified in options
+   */
+  @NotNull
+  private Observable<Optional<File>> _observeNodeJSVersion()
+  {
+    return NodeJSOptions.observe()
+        // extract path from options
+        .map(pOptions -> Optional.ofNullable(pOptions.getPath()))
+        .distinctUntilChanged()
+
+        // check if file exists
+        .map(pPathOpt -> pPathOpt
+            .map(File::new)
+            .filter(File::exists));
+  }
+
+  /**
+   * @return Observable that contains the current package.json
+   */
+  @NotNull
+  private Observable<Optional<File>> _observePackageJson()
+  {
+    assert project != null;
+    return FileObservable.create(new File(project.getProjectDirectory().getPath(), "package.json"));
+  }
+
+  /**
+   * @return Observable that contains the current tsconfig.json
+   */
+  @NotNull
+  private Observable<Optional<File>> _observeTSConfig()
+  {
+    assert project != null;
+    return FileObservable.create(new File(project.getProjectDirectory().getPath(), "tsconfig.json"));
   }
 
 }
