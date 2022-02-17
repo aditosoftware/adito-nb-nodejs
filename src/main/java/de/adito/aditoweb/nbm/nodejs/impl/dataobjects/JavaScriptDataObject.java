810e09384a25779309b52f02227334e03296cd14
m.kaspera <m.kaspera@adito.de>
2022-02-17 11:37:42 +0100

    Feat: The displayed name for JavaScript files is now adjusted to the ADITO project structure

--- src/main/java/de/adito/aditoweb/nbm/nodejs/impl/dataobjects/JavaScriptDataObject.java
+++ src/main/java/de/adito/aditoweb/nbm/nodejs/impl/dataobjects/JavaScriptDataObject.java
@@ -1,8 +1,13 @@
 package de.adito.aditoweb.nbm.nodejs.impl.dataobjects;
 
+import de.adito.aditoweb.nbm.nbide.nbaditointerface.javascript.node.IJSNodeNameProvider;
+import io.reactivex.rxjava3.disposables.*;
+import org.jetbrains.annotations.NotNull;
+import org.netbeans.api.project.*;
 import org.netbeans.modules.textmate.lexer.api.GrammarRegistration;
 import org.openide.filesystems.*;
 import org.openide.loaders.*;
+import org.openide.nodes.*;
 import org.openide.util.NbBundle;
 
 import java.io.IOException;
@@ -21,7 +26,9 @@ import static de.adito.aditoweb.nbm.nodejs.impl.dataobjects.JavaScriptDataObject
 @GrammarRegistration(mimeType = "text/javascript", grammar = "JavaScript.tmLanguage.json")
 public class JavaScriptDataObject extends MultiDataObject
 {
+
   public static final String JS_EXTENSION = "js";
+  private Node nodeDelegate = null;
 
   public JavaScriptDataObject(FileObject pf, MultiFileLoader loader) throws IOException
   {
@@ -30,9 +37,67 @@ public class JavaScriptDataObject extends MultiDataObject
   }
 
   @Override
+  protected Node createNodeDelegate()
+  {
+    if (nodeDelegate == null)
+    {
+      return new JSNodeDelegate(this);
+    }
+    return nodeDelegate;
+  }
+
+  @Override
   protected int associateLookup()
   {
     return 1;
   }
 
+  /**
+   * Node that handles the displayed name for the node in such a way that an sensible name is displayed for the ADITO project structure
+   */
+  private static class JSNodeDelegate extends DataNode implements Disposable
+  {
+    @NotNull
+    private final DataObject dataObject;
+    @NotNull
+    private final CompositeDisposable disposable = new CompositeDisposable();
+    private String displayName = "";
+
+    public JSNodeDelegate(@NotNull DataObject pDataObject)
+    {
+      super(pDataObject, Children.LEAF);
+      dataObject = pDataObject;
+      Project owner = FileOwnerQuery.getOwner(dataObject.getPrimaryFile());
+      IJSNodeNameProvider.findInstance(owner)
+          .ifPresent(pNodeNameProvider -> disposable.add(pNodeNameProvider.getDisplayName(dataObject)
+                                                                                              .subscribe(pNameOpt -> pNameOpt
+                                                                                                  .ifPresent(this::updateDisplayName))));
+    }
+
+    private void updateDisplayName(String pName)
+    {
+      String old = displayName;
+      displayName = pName + "." + JS_EXTENSION;
+      setDisplayName(displayName);
+      fireDisplayNameChange(old, displayName);
+    }
+
+    @Override
+    public String getDisplayName()
+    {
+      return displayName;
+    }
+
+    @Override
+    public void dispose()
+    {
+      disposable.dispose();
+    }
+
+    @Override
+    public boolean isDisposed()
+    {
+      return disposable.isDisposed();
+    }
+  }
 }
