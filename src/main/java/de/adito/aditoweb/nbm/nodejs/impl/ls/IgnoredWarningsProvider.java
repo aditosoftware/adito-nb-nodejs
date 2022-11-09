650347b5f20755863df2970f788ac1ee243b1be5
Pascal Neub <p.neub@adito.de>
2022-11-09 15:35:51 +0100

    fix: Create the ignoredWarnings file only if there are any ignored warnings
    This also solves the problem when deleting the ignoredWarnings file, as this case is treated as if there were no ignored warnings

--- src/main/java/de/adito/aditoweb/nbm/nodejs/impl/ls/IgnoredWarningsProvider.java
+++ src/main/java/de/adito/aditoweb/nbm/nodejs/impl/ls/IgnoredWarningsProvider.java
@@ -2,7 +2,6 @@ package de.adito.aditoweb.nbm.nodejs.impl.ls;
 
 import com.google.gson.Gson;
 import de.adito.aditoweb.nbm.nbide.nbaditointerface.project.IProjectVisibility;
-import de.adito.notification.INotificationFacade;
 import de.adito.observables.netbeans.FileObservable;
 import io.reactivex.rxjava3.core.Observable;
 import org.jetbrains.annotations.*;
@@ -12,6 +11,7 @@ import org.openide.util.lookup.ServiceProvider;
 
 import java.io.*;
 import java.util.*;
+import java.util.logging.*;
 import java.util.stream.Collectors;
 
 /**
@@ -20,6 +20,8 @@ import java.util.stream.Collectors;
 @ServiceProvider(service = IgnoredWarningsProvider.class, path = "Projects/de-adito-project/Lookup")
 public class IgnoredWarningsProvider
 {
+
+  private static final Logger _LOGGER = Logger.getLogger(IgnoredWarningsProvider.class.getName());
 
   private Observable<Set<IgnoredWarningsFacade.WarningsItem>> warningsObs = null;
   private final Project project;
@@ -48,34 +50,26 @@ public class IgnoredWarningsProvider
       throw new IllegalStateException("IgnoredWarningsProvider has to be obtained from the project lookup");
     if (warningsObs == null)
     {
-      try
+      if (Boolean.TRUE.equals(project.getLookup().lookup(IProjectVisibility.class).isVisible()))
       {
-        if (Boolean.TRUE.equals(project.getLookup().lookup(IProjectVisibility.class).isVisible()))
-        {
-          File ignoredWarningsFile = getIgnoredWarningsFile(project);
-          warningsObs = FileObservable.createForPlainFile(ignoredWarningsFile)
-              .map(pFile -> readIgnoredWarnings(ignoredWarningsFile));
-        }
-        else
-        {
-          List<Observable<Set<IgnoredWarningsFacade.WarningsItem>>> warningsItemsObs = new ArrayList<>();
-          Project currentProj = project;
-          boolean breakLoop = false;
-          while (currentProj != null && !breakLoop)
-          {
-            breakLoop = (Boolean.TRUE.equals(currentProj.getLookup().lookup(IProjectVisibility.class).isVisible()));
-            File ignoredWarningsFile = getIgnoredWarningsFile(currentProj);
-            warningsItemsObs.add(FileObservable.createForPlainFile(ignoredWarningsFile)
-                                     .map(pFile -> readIgnoredWarnings(ignoredWarningsFile)));
-            currentProj = FileOwnerQuery.getOwner(currentProj.getProjectDirectory().getParent());
-          }
-          warningsObs = Observable.combineLatest(warningsItemsObs, IgnoredWarningsProvider::combineSetArray);
-        }
+        File ignoredWarningsFile = getIgnoredWarningsFile(project);
+        warningsObs = FileObservable.createForPlainFile(ignoredWarningsFile)
+            .map(pFile -> readIgnoredWarnings(ignoredWarningsFile));
       }
-      catch (IOException pE)
+      else
       {
-        INotificationFacade.INSTANCE.error(pE);
-        warningsObs = Observable.just(Set.of());
+        List<Observable<Set<IgnoredWarningsFacade.WarningsItem>>> warningsItemsObs = new ArrayList<>();
+        Project currentProj = project;
+        boolean breakLoop = false;
+        while (currentProj != null && !breakLoop)
+        {
+          breakLoop = (Boolean.TRUE.equals(currentProj.getLookup().lookup(IProjectVisibility.class).isVisible()));
+          File ignoredWarningsFile = getIgnoredWarningsFile(currentProj);
+          warningsItemsObs.add(FileObservable.createForPlainFile(ignoredWarningsFile)
+                                   .map(pFile -> readIgnoredWarnings(ignoredWarningsFile)));
+          currentProj = FileOwnerQuery.getOwner(currentProj.getProjectDirectory().getParent());
+        }
+        warningsObs = Observable.combineLatest(warningsItemsObs, IgnoredWarningsProvider::combineSetArray);
       }
     }
     return warningsObs;
@@ -97,35 +91,56 @@ public class IgnoredWarningsProvider
         .collect(Collectors.toSet());
   }
 
-  @SuppressWarnings("ResultOfMethodCallIgnored") // ignore for mkdirs and createNewFile
+  /**
+   * Returns the ignored warnings file for the given project
+   *
+   * @param pProject the project
+   * @return the ignored warnings file
+   */
   @NotNull
-  public static File getIgnoredWarningsFile(@NotNull Project pProject) throws IOException
+  public static File getIgnoredWarningsFile(@NotNull Project pProject)
   {
-    File ignoredWarnings = new File(FileUtil.toFile(pProject.getProjectDirectory()), ".aditoprj/ignoredWarnings");
-    if (!ignoredWarnings.exists())
-    {
-      ignoredWarnings.getParentFile().mkdirs();
-      ignoredWarnings.createNewFile();
-    }
-    return ignoredWarnings;
+    return new File(FileUtil.toFile(pProject.getProjectDirectory()), ".aditoprj/ignoredWarnings");
   }
 
-  private static Set<IgnoredWarningsFacade.WarningsItem> readIgnoredWarnings(@Nullable File pIgnoreFile) throws IOException
+  /**
+   * Reads, and parses the provided ignored warnings file into a set of {@link IgnoredWarningsFacade.WarningsItem}
+   *
+   * @param pIgnoreFile the ignored warnings file
+   * @return the set of {@link IgnoredWarningsFacade.WarningsItem}
+   */
+  private static Set<IgnoredWarningsFacade.WarningsItem> readIgnoredWarnings(@Nullable File pIgnoreFile)
   {
     if (pIgnoreFile == null || !pIgnoreFile.exists())
       return Set.of();
     HashSet<IgnoredWarningsFacade.WarningsItem> warningsSet = new HashSet<>();
-    Set<Map.Entry<String, String>> entrySet = Optional.ofNullable(readIgnoredWarningsFileContent(pIgnoreFile))
-        .map(pFileContent -> pFileContent.content)
-        .map(Map::entrySet)
-        .orElse(Set.of());
-    for (Map.Entry<String, String> warningItem : entrySet)
+    try
     {
-      warningsSet.add(new IgnoredWarningsFacade.WarningsItem(Integer.parseInt(warningItem.getKey()), warningItem.getValue()));
+      Set<Map.Entry<String, String>> entrySet = Optional.ofNullable(readIgnoredWarningsFileContent(pIgnoreFile))
+          .map(pFileContent -> pFileContent.content)
+          .map(Map::entrySet)
+          .orElse(Set.of());
+      for (Map.Entry<String, String> warningItem : entrySet)
+      {
+        warningsSet.add(new IgnoredWarningsFacade.WarningsItem(Integer.parseInt(warningItem.getKey()), warningItem.getValue()));
+      }
+    }
+    catch (IOException pE)
+    {
+      _LOGGER.log(Level.WARNING, "Could not read ignored warnings file", pE);
+      return Set.of();
     }
     return warningsSet;
   }
 
+  /**
+   * Reads, and parses the provided ignored warnings file
+   *
+   * @param pIgnoreFile the ignored warnings file
+   * @return the content of the ignored warnings file parsed as {@link IgnoreWarningFix.FileContent} or null if the file is empty
+   * @throws IOException if reading the ignored warnings file failed
+   */
+  @Nullable
   private static IgnoreWarningFix.FileContent readIgnoredWarningsFileContent(@NotNull File pIgnoreFile) throws IOException
   {
     try (FileReader fr = new FileReader(pIgnoreFile))
