1fc73729c5027e6a0bcde266258f508841281ce9
Pascal Neub <p.neub@adito.de>
2022-11-28 08:12:21 +0100

    feat: create NPMCommandExecutor to allow global flag

--- src/main/java/de/adito/aditoweb/nbm/nodejs/impl/parser/PackageParser.java
+++ src/main/java/de/adito/aditoweb/nbm/nodejs/impl/parser/PackageParser.java
@@ -1,10 +1,11 @@
 package de.adito.aditoweb.nbm.nodejs.impl.parser;
 
 import com.google.gson.Gson;
+import de.adito.aditoweb.nbm.nbide.nbaditointerface.javascript.node.INodeJSExecBase;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author w.glanzer, 12.05.2021
@@ -58,11 +59,47 @@ public class PackageParser
   }
 
   /**
+   * Parses the output of "npm list" and returns the binaries,
+   * so that we don't have to use the ".cmd" wrapper.
+   *
+   * @param pJsonData output of "npm list"
+   * @return a map of binaries and their INodeJSExecBase, pointing to the resolved javascript file
+   */
+  @NotNull
+  public static Map<String, INodeJSExecBase> parseBinaries(@NotNull String pJsonData)
+  {
+    try
+    {
+      _Type root = new Gson().fromJson(pJsonData, _Type.class);
+      if (root.dependencies != null)
+      {
+        Map<String, INodeJSExecBase> res = new HashMap<>();
+        for (Map.Entry<String, _Type> dep : root.dependencies.entrySet())
+        {
+          if (dep.getValue().bin == null)
+            continue;
+          for (Map.Entry<String, String> binary : dep.getValue().bin.entrySet())
+            res.put(binary.getKey(), INodeJSExecBase.module(dep.getKey(), binary.getValue()));
+        }
+        return res;
+      }
+    }
+    catch (Exception pE)
+    {
+      // just return empty map
+    }
+
+    return Map.of();
+  }
+
+  /**
    * gson type of package.json
    */
   private static class _Type
   {
+    public Map<String, _Type> dependencies;
     public Map<String, String> scripts;
+    public Map<String, String> bin;
   }
 
 }
