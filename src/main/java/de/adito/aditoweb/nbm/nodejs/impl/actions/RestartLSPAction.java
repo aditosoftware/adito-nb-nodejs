86eb904fdb7464dea66302691a9a4a287200e73e
Ramona Hartinger <r.hartinger@adito.de>
2023-03-08 09:08:00 +0100

    #2016181: added short description

--- src/main/java/de/adito/aditoweb/nbm/nodejs/impl/actions/RestartLSPAction.java
+++ src/main/java/de/adito/aditoweb/nbm/nodejs/impl/actions/RestartLSPAction.java
@@ -7,6 +7,7 @@ import org.netbeans.modules.lsp.client.LSPBindings;
 import org.netbeans.modules.lsp.client.spi.ServerRestarter;
 import org.openide.awt.*;
 
+import javax.swing.*;
 import javax.swing.text.JTextComponent;
 import java.awt.event.ActionEvent;
 
@@ -20,7 +21,7 @@ import java.awt.event.ActionEvent;
     @ActionReference(path = "Editors/text/javascript/Toolbars/Default", position = 20220)
 })
 @ActionID(category = "adito/editor/toolbar", id = "de.adito.aditoweb.nbm.nodejs.impl.actions.RestartLSP")
-@ActionRegistration(displayName = "Restart the server that provides autocomplete, go-to actions and other features for js/ts files",
+@ActionRegistration(displayName = "Restart LSP",
     iconBase = "de/adito/aditoweb/nbm/nodejs/impl/actions/restart.svg", lazy = false)
 public class RestartLSPAction extends BaseAction
 {
@@ -30,8 +31,9 @@ public class RestartLSPAction extends BaseAction
    */
   public RestartLSPAction()
   {
-    super("Restart the server that provides autocomplete, go-to actions and other features for js/ts files");
+    super("Restart LSP");
     putValue(BaseAction.ICON_RESOURCE_PROPERTY, "de/adito/aditoweb/nbm/nodejs/impl/actions/restart.svg");
+    putValue(Action.SHORT_DESCRIPTION, "Restart the server that provides autocomplete, go-to actions and other features for js/ts files");
   }
 
   @Override
