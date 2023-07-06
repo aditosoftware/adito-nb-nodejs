2473968ef1f8cbaef23b093a7cc8abaa08572047
Pascal Neub <p.neub@adito.de>
2023-07-06 13:43:34 +0200

    feat: Improve TypeScript compilation

--- src/main/java/de/adito/aditoweb/nbm/nodejs/impl/typescript/TypeScriptLinkedFileCompiler.java
+++ src/main/java/de/adito/aditoweb/nbm/nodejs/impl/typescript/TypeScriptLinkedFileCompiler.java
@@ -4,7 +4,9 @@ import com.google.gson.*;
 import de.adito.aditoweb.nbm.nbide.nbaditointerface.javascript.node.*;
 import de.adito.aditoweb.nbm.nbide.nbaditointerface.transpiler.ILinkedFileCompiler;
 import de.adito.aditoweb.nbm.nodejs.impl.*;
+import lombok.*;
 import org.jetbrains.annotations.NotNull;
+import org.openide.util.RequestProcessor;
 import org.openide.util.io.*;
 import org.openide.util.lookup.ServiceProvider;
 
@@ -12,7 +14,7 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
 /**
@@ -47,53 +49,114 @@ public final class TypeScriptLinkedFileCompiler implements ILinkedFileCompiler
   @Override
   public CompletableFuture<Void> compileFiles(@NotNull Path pRoot, @NotNull List<Path> pFiles)
   {
+    return CompletableFuture.supplyAsync(() -> createConfig(pRoot, pFiles), RequestProcessor.getDefault())
+        .thenAccept(pTsConfig -> runCompiler(pTsConfig).thenAccept(this::cleanup));
+  }
+
+  /**
+   * Creates a temporary tsconfig file.
+   *
+   * @param pRoot root of the project/module
+   * @param pFiles files that sohuld be included in the compilation
+   * @return path to the tsconfig file
+   */
+  @NotNull
+  private Path createConfig(@NotNull Path pRoot, @NotNull List<Path> pFiles)
+  {
+    Path tsconfig;
+    try
+    {
+      Path extendsTsConfig = pRoot.resolve("tsconfig.json");
+
+      TsConfigJson.CompilerOptions compilerOptions = TsConfigJson.CompilerOptions.builder()
+          .noEmit(false)
+          .checkJs(false)
+          .sourceMap(true)
+          .build();
+
+      TsConfigJson tsConfigJson = TsConfigJson.builder()
+          .extendsPath(Files.exists(extendsTsConfig) ? extendsTsConfig.toString() : null)
+          .files(pFiles.stream().map(Path::toString).collect(Collectors.toList()))
+          .include(List.of())
+          .exclude(List.of())
+          .compilerOptions(compilerOptions)
+          .build();
+
+
+      tsconfig = Files.createTempFile("tsconfig.jdito", ".json");
+      try (final FileWriter writer = new FileWriter(tsconfig.toFile()))
+      {
+        new Gson().toJson(tsConfigJson, TsConfigJson.class, writer);
+      }
+    }
+    catch (IOException pE)
+    {
+      throw new CompilationException(pE);
+    }
+
+    return tsconfig;
+  }
+
+  /**
+   * Runs the TypeScript-Compiler.
+   *
+   * @param pTsConfig tsconfig that should be used as an input to tsc
+   * @return future that resolves once the compiler has finished executing
+   */
+  @NotNull
+  private CompletableFuture<CompilationResult> runCompiler(@NotNull Path pTsConfig)
+  {
     INodeJSExecutor executor = NodeJSInstallation.getCurrent().getExecutor();
     INodeJSEnvironment bundledEnvironment = NodeJSInstallation.getCurrent().getEnvironment();
     INodeJSExecBase tscBinary = new NPMCommandExecutor(executor, bundledEnvironment, true, null).binaries().get("tsc");
     String tscBinaryPath = bundledEnvironment.resolveExecBase(tscBinary).getAbsolutePath();
 
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
     try
     {
-      TsConfigJson tsConfigJson = new TsConfigJson();
-      tsConfigJson.files = pFiles.stream().map(Path::toString).collect(Collectors.toList());
-      tsConfigJson.include = List.of();
-      tsConfigJson.exclude = List.of("**/*");
-      tsConfigJson.compilerOptions = new TsConfigJson.CompilerOptions();
-      tsConfigJson.compilerOptions.noEmit = false;
-      tsConfigJson.compilerOptions.checkJs = false;
-      tsConfigJson.compilerOptions.sourceMap = true;
-
-      Path extendsTsConfig = pRoot.resolve("tsconfig.json");
-      if (Files.exists(extendsTsConfig))
-        tsConfigJson.extendsPath = extendsTsConfig.toString();
-
-      Path tsconfig = Files.createTempFile("tsconfig.jdito", ".json");
-      try (final FileWriter writer = new FileWriter(tsconfig.toFile()))
-      {
-        new Gson().toJson(tsConfigJson, TsConfigJson.class, writer);
-      }
-
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      return executor.executeAsync(bundledEnvironment, INodeJSExecBase.node(), baos, baos, new NullInputStream(), tscBinaryPath, "--build", tsconfig.toString())
-          .thenApply(pExitCode -> {
-            // Don't delete the temporary tsconfig, if an error occurred. This is intentional.
-            if (pExitCode != 0)
-              throw new CompilationException(baos.toString(StandardCharsets.UTF_8));
-
-            try
-            {
-              Files.delete(tsconfig);
-            }
-            catch (IOException pE)
-            {
-              throw new RuntimeException(pE);
-            }
-            return null;
-          });
+      return executor.executeAsync(
+              bundledEnvironment,
+              INodeJSExecBase.node(),
+              baos, baos, new NullInputStream(),
+              tscBinaryPath, "--build", pTsConfig.toString())
+          .thenApply(pExitCode -> new CompilationResult(pTsConfig, baos, pExitCode));
     }
     catch (IOException pE)
     {
-      throw new RuntimeException(pE);
+      throw new CompilationException(pE);
     }
+  }
+
+  /**
+   * Cleans up the temporary tsconfig file and handles compiler errors
+   *
+   * @param pResult result of the compilation
+   */
+  private void cleanup(@NotNull CompilationResult pResult)
+  {
+    // Don't delete the temporary tsconfig, if an error occurred. This is intentional.
+    if (pResult.getExitCode() != 0)
+      throw new CompilationException(pResult.getBaos().toString(StandardCharsets.UTF_8));
+
+    try
+    {
+      Files.delete(pResult.getTsconfig());
+    }
+    catch (IOException pE)
+    {
+      throw new CompilationException(pE);
+    }
+  }
+
+  /**
+   * Represents the result of a compilation
+   */
+  @AllArgsConstructor
+  @Getter
+  private static class CompilationResult
+  {
+    private final Path tsconfig;
+    private final ByteArrayOutputStream baos;
+    private final int exitCode;
   }
 }
